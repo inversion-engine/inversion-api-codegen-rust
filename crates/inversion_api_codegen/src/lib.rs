7d94dbaebfa526dd03ad374c371ab3484d90b213
neonphog <neonphog@gmail.com>
2020-10-26 16:44:57 -0600

    checkpoint

--- crates/inversion_api_codegen/src/lib.rs
+++ crates/inversion_api_codegen/src/lib.rs
@@ -44,6 +44,15 @@ fn block_comment_close() -> TokenStream {
     ts
 }
 
+fn check_gen_primitive(ty: &Type) -> Option<TokenStream> {
+    match ty {
+        Type::Bool { .. } => Some(quote!(bool)),
+        Type::U32 { .. } => Some(quote!(u32)),
+        Type::String { .. } => Some(quote!(String)),
+        _ => None,
+    }
+}
+
 fn gen_one_type(name: Ident, ty: &Type) -> TokenStream {
     let mod_name = format_ident!("{}", name.to_string().to_snake_case());
     let mut mod_tokens = TokenStream::new();
@@ -77,13 +86,20 @@ fn gen_one_type(name: Ident, ty: &Type) -> TokenStream {
             let type_names = content.iter().map(|i| {
                 let doc = i.content.doc().as_ref().map(|s| s.as_str()).unwrap_or("");
                 let name = format_ident!("{}{}", name, i.index);
-                let res = gen_one_type(name.clone(), &i.content);
                 let open = block_comment_open();
                 let close = block_comment_close();
-                mod_tokens.extend(res);
-                quote! {
-                    #open #doc #close
-                    #mod_name::#name,
+                if let Some(p) = check_gen_primitive(&i.content) {
+                    quote! {
+                        #open #doc #close
+                        #p,
+                    }
+                } else {
+                    let res = gen_one_type(name.clone(), &i.content);
+                    mod_tokens.extend(res);
+                    quote! {
+                        #open #doc #close
+                        #mod_name::#name,
+                    }
                 }
             });
             item_tokens.extend(quote! {
@@ -95,20 +111,58 @@ fn gen_one_type(name: Ident, ty: &Type) -> TokenStream {
             let doc = doc.as_ref().map(|s| s.as_str()).unwrap_or("");
             let mut content = (*content).iter().map(|v| v.clone()).collect::<Vec<_>>();
             content.sort_unstable_by_key(|k| k.1.index);
+            struct D<'a> {
+                doc: &'a str,
+                tname: Ident,
+                ty: TokenStream,
+            }
             let types = content.iter().map(|i| {
                 let doc = i.1.content.doc().as_ref().map(|s| s.as_str()).unwrap_or("");
                 let tname = format_ident!("{}", i.0.to_snake_case());
                 let name = format_ident!("{}", i.0.to_pascal_case());
-                let res = gen_one_type(name.clone(), &i.1.content);
-                mod_tokens.extend(res);
+                if let Some(p) = check_gen_primitive(&i.1.content) {
+                    D { doc, tname, ty: p }
+                } else {
+                    let res = gen_one_type(name.clone(), &i.1.content);
+                    mod_tokens.extend(res);
+                    D { doc, tname, ty: quote!(#mod_name::#name) }
+                }
+            }).collect::<Vec<_>>();
+            let types1 = types.iter().map(|d| {
+                let D { doc, tname, ty } = d;
                 quote! {
                     #[doc = #doc]
-                    #tname: #mod_name::#name
+                    #tname: #ty,
+                }
+            });
+            let types2 = types.iter().map(|d| {
+                let D { ty, .. } = d;
+                quote! {
+                    &#ty,
+                }
+            });
+            let types3 = types.iter().map(|d| {
+                let D { tname, .. } = d;
+                quote! {
+                    &self.#tname,
                 }
             });
             item_tokens.extend(quote! {
                 #[doc = #doc]
-                pub struct #name { #(#types,)* }
+                #[derive(Debug, Clone, PartialEq)]
+                pub struct #name {
+                    #(#types1)*
+                }
+
+                impl ::serde::Serialize for #name {
+                    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+                    where
+                        S: ::serde::Serializer,
+                    {
+                        let r: (#(#types2)*) = (#(#types3)*);
+                        r.serialize(serializer)
+                    }
+                }
             });
         }
         _ => (),
@@ -163,26 +217,40 @@ mod tests {
     "unstableFeatures": {},
     "types": {
       "error": {
+        "doc": "error type",
         "type": "string"
       },
       "callOne": {
+        "doc": "a tuple type",
         "type": "tuple",
         "content": [
             {
                 "index": 0,
-                "content": { "type": "bool" }
+                "content": { "doc": "first tuple item", "type": "bool" }
             },
             {
                 "index": 1,
-                "content": { "type": "u32" }
+                "content": { "doc": "second", "type": "u32" }
             }
         ]
       },
       "callTwo": {
+        "doc": "a struct type",
         "type": "struct",
         "content": {
-          "yay": { "index": 0, "content": { "type": "bool" } },
-          "age": { "index": 0, "content": { "type": "u32" } }
+          "yay": { "index": 0, "content": { "doc": "yay", "type": "bool" } },
+          "age": { "index": 1, "content": { "doc": "age", "type": "u32" } },
+          "sub": {
+            "index": 2,
+            "content": {
+              "doc": "a sub struct",
+              "type": "struct",
+              "content": {
+                "yay": { "index": 0, "content": { "doc": "yay", "type": "bool" } },
+                "age": { "index": 1, "content": { "doc": "age", "type": "u32" } }
+              }
+            }
+          }
         }
       }
     },
